3bd349c5e35ff110c049d44b7d658d7cf5684fb2
Fredrik Domert Eriksson <fredrik.domert@gmail.com>
2024-11-29 14:52:50 +0100

    Uppdatering

--- WIndchillprojektet/Program.cs
+++ WIndchillprojektet/Program.cs
@@ -4,22 +4,59 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hej och välkommen till Windchillkalkylatorn!");
-        Console.Write("Skriv in temperaturen i grader Celsius: ");
-        double userTemp = double.Parse(Console.ReadLine());
-        
-        Console.Write("Skriv vindhastigheten i m/s: ");
-        double userWindSpeedMs = double.Parse(Console.ReadLine());
-        
-        double userWindSpeedKmh = userWindSpeedMs * 3.6;
-        
-        double wct = 13.12 + 0.6215 * userTemp - 11.37 * Math.Pow(userWindSpeedKmh, 0.16) + 0.3965 * userTemp * Math.Pow(userWindSpeedKmh, 0.16);
 
-        
-        Console.Write(wct.ToString("0.0"));
-        
-        
-        
-        
+        bool isRunning = true;
+
+        while (isRunning)
+        {
+            
+            Console.WriteLine("\nHej och välkommen till Windchillkalkylatorn!");
+            Console.WriteLine("\n1. Vill du beräkna windchill-faktor?");
+            Console.WriteLine("2. Avsluta");
+            // Ändra så att man kan skriva in en bokstav.
+            int userChoice = int.Parse(Console.ReadLine());
+
+
+            switch (userChoice)
+            {
+                case 1:
+                    Console.Clear();
+                    Console.Write("Skriv in temperaturen i grader Celsius: ");
+                    double userTemp = double.Parse(Console.ReadLine());
+
+                    Console.Write("Skriv vindhastigheten i m/s: ");
+                    double userWindSpeedMs = double.Parse(Console.ReadLine());
+
+                    double userWindSpeedKmh = userWindSpeedMs * 3.6;
+
+                    double wct = 13.12 + 0.6215 * userTemp - 11.37 * Math.Pow(userWindSpeedKmh, 0.16) +
+                                 0.3965 * userTemp * Math.Pow(userWindSpeedKmh, 0.16);
+
+                    
+                    Console.Write("\n" + wct.ToString("0.0"));
+                    Thread.Sleep(1750);
+                    Console.WriteLine();
+                    
+                    break;
+                
+                case 2:
+                    
+                    Console.Clear();
+                    Console.WriteLine("\nDu valde att avsluta.");
+                    isRunning = false;
+                    break;
+                
+            }
+            
+                
+                
+                
+            
+                
+                
+        }
+
+
+
     }
 }
