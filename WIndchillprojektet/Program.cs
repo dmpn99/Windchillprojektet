2672058a63858022cac7c000e3411b7af7833536
Fredrik Domert Eriksson <fredrik.domert@gmail.com>
2024-11-29 11:21:14 +0100

    Update.
    Första uppdateringen.

--- WIndchillprojektet/Program.cs
+++ WIndchillprojektet/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hej Pärra!");
+        Console.WriteLine("Hej och välkommen till Windchillkalkylatorn!");
+        Console.Write("Skriv in temperaturen i grader Celsius: ");
+        double userTemp = double.Parse(Console.ReadLine());
+        
+        Console.Write("Skriv vindhastigheten i m/s: ");
+        double userWindSpeedMs = double.Parse(Console.ReadLine());
+        
+        double userWindSpeedKmh = userWindSpeedMs * 3.6;
+        
+        double wct = 13.12 + 0.6215 * userTemp - 11.37 * userWindSpeedKmh^0.16 + 0.3965 * userTemp * userWindSpeedKmh^0.16;
+        
+        Console.Write(wct);
+        
+        
+        
+        
     }
 }
